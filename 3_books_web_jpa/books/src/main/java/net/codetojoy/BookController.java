06b007ad3497548cf5c37511db8441c60a7d418e
Michael Easter <codetojoy@gmail.com>
2016-12-15 23:26:47 -0400

    incremental change as part of a larger frenzy of activity

--- 3_books_web_jpa/books/src/main/java/net/codetojoy/BookController.java
+++ 3_books_web_jpa/books/src/main/java/net/codetojoy/BookController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.*;
 
 @Controller
